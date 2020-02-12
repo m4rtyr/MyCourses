6415838244078f225708e246e6806b71ba720333
m4rtyr <ssharma10393@gmail.com>
2020-02-11 23:48:17 -0600

    Added starter files for KD Trees Assignment

---
+++ AlgorithmsI/assignments/KDTrees/KdTree.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: S. Sharma <m4rtyr>
+ * @Date:   2020-02-11T23:43:09-06:00
+ * @Email:  silentcat@protonmail.com
+ * @Last modified by:   m4rtyr
+ * @Last modified time: 2020-02-11T23:47:44-06:00
+ */
+
+public class KdTree
+{
+  
+}
---
+++ AlgorithmsI/assignments/KDTrees/PointSET.java
@@ -0,0 +1,48 @@
+/**
+ * @Author: S. Sharma <m4rtyr>
+ * @Date:   2020-02-11T23:43:12-06:00
+ * @Email:  silentcat@protonmail.com
+ * @Last modified by:   m4rtyr
+ * @Last modified time: 2020-02-11T23:47:05-06:00
+ */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+  public PointSET() {
+
+  }
+
+  public boolean isEmpty() {
+
+  }
+
+  public int size() {
+
+  }
+
+  public void insert(Point2D p) {
+
+  }
+
+  public boolean contains(Point2D p) {
+
+  }
+
+  public void draw() {
+
+  }
+
+  public Iterable<Point2D> range(RectHV rect) {
+
+  }
+
+  public Point2D nearest(Point2D p) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
